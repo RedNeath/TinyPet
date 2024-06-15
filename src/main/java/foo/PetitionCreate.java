804468a380eba22c11e06c44a4203e9359955089
RedNeath <theandre.bourry@ik.me>
2024-06-15 14:36:44 +0000

    [FEAT] Sign a petition

--- src/main/java/foo/PetitionCreate.java
+++ src/main/java/foo/PetitionCreate.java
@@ -3,7 +3,6 @@ package foo;
 import java.util.List;
 
 public class PetitionCreate {
-    public String author;
     public String name;
     public String description;
     public int signatureTarget;
--- src/main/java/foo/PetitionEndpoint.java
+++ src/main/java/foo/PetitionEndpoint.java
@@ -1,40 +1,26 @@
 package foo;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Random;
-import java.util.Set;
 import java.util.stream.Collectors;
 
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 
 import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.EntityNotFoundException;
 import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
-import com.google.appengine.api.datastore.KeyRange;
 import com.google.appengine.api.datastore.PreparedQuery;
-import com.google.appengine.api.datastore.PropertyProjection;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.SortDirection;
-import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
 import com.google.appengine.api.datastore.Query.FilterOperator;
 import com.google.appengine.api.datastore.Query.FilterPredicate;
-
-import com.google.appengine.repackaged.com.google.datastore.v1.CompositeFilter;
-import com.google.appengine.repackaged.com.google.datastore.v1.Projection;
-import com.google.appengine.repackaged.com.google.datastore.v1.PropertyFilter;
 
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
@@ -60,13 +46,14 @@ import com.google.appengine.api.datastore.Transaction;
 )
 public class PetitionEndpoint {
 	@ApiMethod(name="createPetition", httpMethod=HttpMethod.POST)
-	public Entity createPetition(PetitionCreate petitionCreate) {
+	public Entity createPetition(PetitionCreate petitionCreate, HttpServletRequest request) {
 		Calendar cal = Calendar.getInstance(); 
 		cal.add(Calendar.MONTH, 6);
 		Date endDate = cal.getTime();
+		String email = request.getHeader("Email");
 
 		Entity petition = new Entity("Petition");
-		petition.setProperty("author", petitionCreate.author);
+		petition.setProperty("author", email);
 		petition.setProperty("name", petitionCreate.name);
 		petition.setProperty("description", petitionCreate.description);
 		petition.setProperty("signature_target", petitionCreate.signatureTarget);
@@ -141,47 +128,23 @@ public class PetitionEndpoint {
 		return pq.asList(FetchOptions.Builder.withDefaults());
 	}
 
+	@ApiMethod(name="signPetition", httpMethod=HttpMethod.POST)
+	public Entity signPetition(@Named("petition") long petition, HttpServletRequest request) {
+		// Has to be connected via google!
+		// Payload payload = ...
+		String email = request.getHeader("Email");
 
-	/* @Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		Entity signature = new Entity("Signature");
+		signature.setProperty("date", new Date());
+		signature.setProperty("petition", petition);
+		// signature.setProperty("user", payload.getEmail());
+		signature.setProperty("user", email);
 
-		response.setContentType("text/html");
-		response.setCharacterEncoding("UTF-8");
+		DatastoreService datastoreService = DatastoreServiceFactory.getDatastoreService();
+		Transaction transaction = datastoreService.beginTransaction();
+		datastoreService.put(signature);
+		transaction.commit();
 
-
-		response.getWriter().print("<h2> finall 5 PU where key > P0 </h2>");
-
-		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-		Key k = KeyFactory.createKey("PU", "P0");
-
-		Query q = new Query("PU").setFilter(new FilterPredicate(Entity.KEY_RESERVED_PROPERTY, FilterOperator.GREATER_THAN, k));
-
-		PreparedQuery pq = datastore.prepare(q);
-		List<Entity> result = pq.asList(FetchOptions.Builder.withLimit(5));
-
-		response.getWriter().print("<li> result:" + result.size() + "<br>");
-		Entity last=null;
-		for (Entity entity : result) {
-			response.getWriter().print("<li>" + entity.getKey());
-			last=entity;
-		}
-
-		response.getWriter().print("<h2> Great, get the next 10 results now </h2>");
-
-		
-		// One way to paginate...
-		q = new Query("PU").setFilter(new FilterPredicate("__key__", FilterOperator.GREATER_THAN, last.getKey()));
-
-		pq = datastore.prepare(q);
-		result = pq.asList(FetchOptions.Builder.withLimit(10));
-
-		response.getWriter().print("<li> result:" + result.size() + "<br>");
-		last=null;
-		for (Entity entity : result) {
-			response.getWriter().print("<li>" + entity.getKey());
-			last=entity;
-		}
-
-		
-	} */
+		return signature;
+	}
 }
